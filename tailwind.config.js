79c8e2c8850390a34eb3e8624bee5330c2f73fde
Shane Ferneyman <s@ferndez.com>
2025-06-15 21:01:16 +1000

    feat: add content path for Tailwind CSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./docs/index.html", // Path relative to the tailwind.config.js file
+  ],
   theme: {
     extend: {},
   },
